541a76745b7f0aa565c66668f40c7b49865f8a75
TAMAL BAG <87414354+tamalbag117@users.noreply.github.com>
2021-08-13 11:55:03 +0530

    Update 01_ASSignment_withoutVariableSwap.cpp

--- 01_ASSignment_withoutVariableSwap.cpp
+++ 01_ASSignment_withoutVariableSwap.cpp
@@ -6,9 +6,9 @@ int main()
 int a,b;
 printf("before swap (a,b) is \n");
 scanf("%d%d",&a,&b);
-a=a+b;
-b=a-b;
-a=a-b;
+a=a ^ b;
+b=a ^ b;
+a=a ^ b;
 printf("swap(%d,%d)\nafter swap a=%d b=%d",b,a,a,b);
 return 0;
 }
